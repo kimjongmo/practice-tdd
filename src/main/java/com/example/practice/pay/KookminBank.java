8a6e5da98a5732783a562c53a6840b16ddc494d2
kimjongmo <kfmd1008@naver.com>
2019-09-07 23:23:14 +0900

    순수 함수 만들기

--- src/main/java/com/example/practice/pay/KookminBank.java
+++ src/main/java/com/example/practice/pay/KookminBank.java
@@ -1,22 +1,35 @@
 package com.example.practice.pay;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
+@Slf4j
 public class KookminBank extends Bank {
     private Map<Long, Account> accountList = new HashMap<>();
     private Long ACCOUNT_ID_GENERATE = 1000L;
 
     @Override
     public PayResponse payProcess(Long senderId, Long receiverId, Long price) {
+        log.info("senderId = {}, receiverId = {}, price = {}", senderId, receiverId, price);
+
+        if (!(isPresentAccount(senderId) && isPresentAccount(receiverId)))
+            return new PayResponse(PayStatus.NOT_FOUND_ACCOUNT);
+
         Account sender = accountList.get(senderId);
         Account receiver = accountList.get(receiverId);
         boolean result = sender.sendTo(receiver, price);
         PayStatus status = result ? PayStatus.PAY_SUCCESS : PayStatus.PAY_FAIL;
         return new PayResponse(status);
+    }
+
+    public boolean isPresentAccount(Long id) {
+        if (!accountList.containsKey(id))
+            return false;
+        return true;
     }
 
     public Long addAccount() {
--- src/main/java/com/example/practice/pay/PaymentGateway.java
+++ src/main/java/com/example/practice/pay/PaymentGateway.java
@@ -7,7 +7,11 @@ public class PaymentGateway {
     private Map<String, Bank> bkList = new HashMap<>();
 
     public PayResponse request(PayRequest request) {
-        return null;
+        if (!(isEmpty(request) && isRightInfo(request)))
+            return new PayResponse(PayStatus.INVALID_INFO);
+
+        Bank bank = bkList.get(request.getBkCode());
+        return bank.payProcess(request.getSenderAccountId(), request.getReceiveAccountId(), request.getAmount());
     }
 
     public void addBank(String bkCode, Bank bank) {
@@ -23,5 +27,22 @@ public class PaymentGateway {
 
     public void setBkList(Map<String, Bank> bkList) {
         this.bkList = bkList;
+    }
+
+    public boolean isEmpty(PayRequest payRequest) {
+        if (payRequest.getAmount() == null
+                || payRequest.getBkCode() == null
+                || payRequest.getPayMethod() == null
+                || payRequest.getSenderAccountId() == null
+                || payRequest.getReceiveAccountId() == null)
+            return false;
+        return true;
+    }
+
+    public boolean isRightInfo(PayRequest payRequest) {
+        if (payRequest.getAmount() < 0
+                || !bkList.containsKey(payRequest.getBkCode()))
+            return false;
+        return true;
     }
 }
